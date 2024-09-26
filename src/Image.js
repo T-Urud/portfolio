075dd4f4d3bfd47a6cc9bd25ef1a4ad1e2d95d15
urud.theo@gmail.com <urud.theo@gmail.com>
2024-09-26 17:04:29 +0200

    new logo

--- src/Image.js
+++ src/Image.js
@@ -2,7 +2,7 @@ import Bluel from "./assets/img/screen-Bluel-project.webp";
 import Kasa from "./assets/img/Kasa-Logo.webp";
 import Mvg from "./assets/img/Mvg-Logo.webp";
 import Card from "./assets/img/screen-Card-project.webp";
-import Le14 from "./assets/img/screen-MVG-project.webp";
+import Le14 from "./assets/img/Logo-Le14.webp";
 import IntercativeBtn from "./assets/img/screen-interactiveBtn2.webp";
 
 export default [Bluel, Kasa, Mvg, Card, Le14, IntercativeBtn];

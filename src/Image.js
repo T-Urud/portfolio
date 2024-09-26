d7ff09388e1b9ebafafcb9033d2f60f6003984f7
urud.theo@gmail.com <urud.theo@gmail.com>
2024-09-26 16:50:17 +0200

    project db modified

--- src/Image.js
+++ src/Image.js
@@ -3,6 +3,6 @@ import Kasa from "./assets/img/Kasa-Logo.webp";
 import Mvg from "./assets/img/Mvg-Logo.webp";
 import Card from "./assets/img/screen-Card-project.webp";
 import Le14 from "./assets/img/screen-MVG-project.webp";
-import Resto from "./assets/img/Logo-resto.webp";
+import IntercativeBtn from "./assets/img/screen-interactiveBtn2.webp";
 
-export default [Bluel, Kasa, Mvg, Card, Le14, Resto];
+export default [Bluel, Kasa, Mvg, Card, Le14, IntercativeBtn];

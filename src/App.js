cf42dbd62d5ef84ee4230a46f23a3dfe9be999e6
urud.theo@gmail.com <urud.theo@gmail.com>
2024-09-27 11:38:48 +0200

    add router in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-hash-link";
 import Header from "./components/Header";
 import TopSection from "./components/TopSection";
 import Footer from "./components/Footer";
@@ -6,16 +7,18 @@ import AnimatedCard from "./components/AnimatedCard";
 
 const App = () => {
   return (
-    <div className="mx-auto px-5 lg:px-12">
-      <Header />
-      <main>
-        <TopSection />
-        <section id="project">
-          <AnimatedCard />
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="mx-auto px-5 lg:px-12">
+        <Header />
+        <main>
+          <TopSection />
+          <section id="project">
+            <AnimatedCard />
+          </section>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
